53d48f3b10d3ffabd51038cc0766932bc9cee4f8
Tim Buckland <tim.buckland@totaljobsgroup.com>
2018-05-30 22:04:58 +0100

    Django dev server appears to be handling multiple requests at the same time when run from webinject command line - but not Chrome - why is that?!

---
+++ tests/multi.bat
@@ -0,0 +1,8 @@
+start perl ..\..\WebInject-Framework\wif.pl ../webinject-server/tests/_multi_1.xml
+timeout 2
+start perl ..\..\WebInject-Framework\wif.pl ../webinject-server/tests/_multi_1.xml
+timeout 2
+start perl ..\..\WebInject-Framework\wif.pl ../webinject-server/tests/_multi_1.xml
+timeout 2
+
+perl ..\..\WebInject-Framework\wif.pl ../webinject-server/tests/_multi_2.xml
